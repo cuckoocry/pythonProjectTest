647de3062d169470f13fa44d3ab4bbd59a49db9f
leo <nuvolebianchejiang@gmail.com>
2025-04-07 13:53:32 +0800

    一些模拟

--- test01/copy.py
+++ test01/copy.py
@@ -1,29 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
 
 
-# pip install requests beautifulsoup4
+if __name__ == '__main__':
+    send_data = [[
+        {
+            "sysCode": "system1",
+            "sendTitle": "通知",
+            "templateId": "template123",
+            "templateKeyMap": {"key1": "value1", "key2": "value2"},
+            "receiveBy": "user1"
+        },
+        {
+            "sysCode": "system2",
+            "sendTitle": "提醒",
+            "templateId": "template456",
+            "templateKeyMap": {"key1": "value3", "key2": "value4"},
+            "receiveBy": "user2"
+        }
+    ]]
+    print(send_data)
 
-# 目标网站的URL
-url = "https://docs.python.org/zh-cn/3/tutorial/introduction.html"
 
-# 发送HTTP GET请求
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # 检查请求是否成功
-except requests.exceptions.RequestException as e:
-    print(f"请求失败: {e}")
-    exit()
 
-# 解析HTML内容
-soup = BeautifulSoup(response.text, 'html.parser')
 
-# 提取网页标题
-title = soup.title.string
-print(f"网页标题: {title}")
-
-# 提取所有链接
-links = soup.find_all('a', href=True)
-print("网页中的链接:")
-for link in links:
-    print(f"{link.text}: {link['href']}")
